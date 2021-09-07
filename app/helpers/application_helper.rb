4f1fb221ca93c5dfa9bf7798965ac947f92e18c3
Raphael1894 <rapha.pereira.perso@gmail.com>
2021-09-07 18:25:43 +0200

    add is_admin? to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def check_if_admin(current_user)
+    if current_user .is_a? Integer
+      check_user=current_user
+    else 
+      check_user = current_user.id
+    end
+      check_user = User.find(check_user)
+      if check_user.is_admin == true
+        return true
+      end
+  end
 end
